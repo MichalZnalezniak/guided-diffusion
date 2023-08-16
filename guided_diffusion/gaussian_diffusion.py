721f098fdb6181c7be9619dbcb78673e7133c1e2
MichalZnalezniak <mznalezn@amd.com>
2023-08-16 20:52:06 +0200

    Clean image guidance

--- guided_diffusion/gaussian_diffusion.py
+++ guided_diffusion/gaussian_diffusion.py
@@ -13,8 +13,9 @@ import torch as th
 
 from .nn import mean_flat
 from .losses import normal_kl, discretized_gaussian_log_likelihood
-
-
+import torchvision
+from PIL import Image
+import torch
 def get_named_beta_schedule(schedule_name, num_diffusion_timesteps):
     """
     Get a pre-defined beta schedule for the given name.
@@ -380,9 +381,21 @@ class GaussianDiffusion:
         """
         alpha_bar = _extract_into_tensor(self.alphas_cumprod, t, x.shape)
 
+        if t[0].item() % 50 == 0:
+            sample_temp = ((p_mean_var['pred_xstart'] + 1) * 127.5).clamp(0, 255).to(th.uint8)
+            sample_temp = sample_temp.permute(0, 2, 3, 1)
+            sample_temp = sample_temp.contiguous()
+            image = Image.fromarray(sample_temp[0].cpu().numpy())
+            image.save(f'./pred_xstart_iter{t[0].item()}.png')
+
+
         eps = self._predict_eps_from_xstart(x, t, p_mean_var["pred_xstart"])
+        clean_image = p_mean_var["pred_xstart"]
+        cond_fn_input = {}
+        cond_fn_input['x'] = x
+        cond_fn_input['pred_xstart'] = clean_image
         eps = eps - (1 - alpha_bar).sqrt() * cond_fn(
-            x, self._scale_timesteps(t), **model_kwargs
+            cond_fn_input, self._scale_timesteps(t), **model_kwargs
         )
 
         out = p_mean_var.copy()
@@ -550,16 +563,18 @@ class GaussianDiffusion:
 
         Same usage as p_sample().
         """
-        out = self.p_mean_variance(
-            model,
-            x,
-            t,
-            clip_denoised=clip_denoised,
-            denoised_fn=denoised_fn,
-            model_kwargs=model_kwargs,
-        )
-        if cond_fn is not None:
-            out = self.condition_score(cond_fn, out, x, t, model_kwargs=model_kwargs)
+        with th.enable_grad():
+            x = x.detach().requires_grad_(True) # Enable grad calculation for x
+            out = self.p_mean_variance(
+                model,
+                x,
+                t,
+                clip_denoised=clip_denoised,
+                denoised_fn=denoised_fn,
+                model_kwargs=model_kwargs,
+            )
+            if cond_fn is not None:
+                out = self.condition_score(cond_fn, out, x, t, model_kwargs=model_kwargs)
 
         # Usually our model outputs epsilon, but we re-derive it
         # in case we used x_start or x_prev prediction.
@@ -683,7 +698,6 @@ class GaussianDiffusion:
         else:
             img = th.randn(*shape, device=device)
         indices = list(range(self.num_timesteps))[::-1]
-
         if progress:
             # Lazy import so that we don't depend on tqdm.
             from tqdm.auto import tqdm
@@ -692,19 +706,18 @@ class GaussianDiffusion:
 
         for i in indices:
             t = th.tensor([i] * shape[0], device=device)
-            with th.no_grad():
-                out = self.ddim_sample(
-                    model,
-                    img,
-                    t,
-                    clip_denoised=clip_denoised,
-                    denoised_fn=denoised_fn,
-                    cond_fn=cond_fn,
-                    model_kwargs=model_kwargs,
-                    eta=eta,
-                )
-                yield out
-                img = out["sample"]
+            out = self.ddim_sample(
+                model,
+                img,
+                t,
+                clip_denoised=clip_denoised,
+                denoised_fn=denoised_fn,
+                cond_fn=cond_fn,
+                model_kwargs=model_kwargs,
+                eta=eta,
+            )
+            yield out
+            img = out["sample"]
 
     def _vb_terms_bpd(
         self, model, x_start, x_t, t, clip_denoised=True, model_kwargs=None
@@ -890,7 +903,6 @@ class GaussianDiffusion:
             "xstart_mse": xstart_mse,
             "mse": mse,
         }
-
 
 def _extract_into_tensor(arr, timesteps, broadcast_shape):
     """
--- scripts/classifier_sample.py
+++ scripts/classifier_sample.py
@@ -2,15 +2,14 @@
 Like image_sample.py, but use a noisy image classifier to guide the sampling
 process towards more realistic images.
 """
-
+from PIL import Image
 import argparse
 import os
-
 import numpy as np
 import torch as th
 import torch.distributed as dist
 import torch.nn.functional as F
-
+import torchvision
 from guided_diffusion import dist_util, logger
 from guided_diffusion.script_util import (
     NUM_CLASSES,
@@ -21,32 +20,28 @@ from guided_diffusion.script_util import (
     add_dict_to_argparser,
     args_to_dict,
 )
-
+from vgg_perceptual_loss import VGGPerceptualLoss
 
 def main():
     args = create_argparser().parse_args()
 
-    dist_util.setup_dist()
+    # dist_util.setup_dist()
     logger.configure()
 
     logger.log("creating model and diffusion...")
     model, diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
-    model.load_state_dict(
-        dist_util.load_state_dict(args.model_path, map_location="cpu")
-    )
-    model.to(dist_util.dev())
+    model.load_state_dict(th.load(args.model_path))
     if args.use_fp16:
         model.convert_to_fp16()
+    model.cuda()
     model.eval()
 
     logger.log("loading classifier...")
     classifier = create_classifier(**args_to_dict(args, classifier_defaults().keys()))
-    classifier.load_state_dict(
-        dist_util.load_state_dict(args.classifier_path, map_location="cpu")
-    )
-    classifier.to(dist_util.dev())
+    classifier.load_state_dict(th.load(args.classifier_path))
+    classifier.cuda()
     if args.classifier_use_fp16:
         classifier.convert_to_fp16()
     classifier.eval()
@@ -54,12 +49,28 @@ def main():
     def cond_fn(x, t, y=None):
         assert y is not None
         with th.enable_grad():
-            x_in = x.detach().requires_grad_(True)
-            logits = classifier(x_in, t)
+            x_in = x['x']
+            clean_image = x['pred_xstart']
+            logits = classifier(clean_image, t)
             log_probs = F.log_softmax(logits, dim=-1)
             selected = log_probs[range(len(logits)), y.view(-1)]
-            return th.autograd.grad(selected.sum(), x_in)[0] * args.classifier_scale
+            print(selected)
+            k = th.autograd.grad(selected.sum(), x_in)[0]
+            return k * args.classifier_scale
 
+    def cond_fn_vgg(x, t, y=None):
+        assert y is not None
+        y = th.zeros(x['x'].shape) # GUIDE TO BLACK IMAGE
+        y = (2*y) - 1
+        y = y.cuda()
+        with th.enable_grad():
+            x_in = x['x']
+            clean_image = x['pred_xstart']
+            loss = th.nn.L1Loss(reduction='mean')(clean_image, y)
+            print(loss)
+            grads = th.autograd.grad(loss, x_in)[0]
+            return grads * args.classifier_scale
+        
     def model_fn(x, t, y=None):
         assert y is not None
         return model(x, t, y if args.class_cond else None)
@@ -70,43 +81,41 @@ def main():
     while len(all_images) * args.batch_size < args.num_samples:
         model_kwargs = {}
         classes = th.randint(
-            low=0, high=NUM_CLASSES, size=(args.batch_size,), device=dist_util.dev()
+            low=99, high=100, size=(args.batch_size,), device=th.device('cuda')
         )
+        print(classes)
         model_kwargs["y"] = classes
         sample_fn = (
-            diffusion.p_sample_loop if not args.use_ddim else diffusion.ddim_sample_loop
+            diffusion.p_sample_loop if not args.use_ddim else  diffusion.ddim_sample_loop
         )
         sample = sample_fn(
             model_fn,
             (args.batch_size, 3, args.image_size, args.image_size),
             clip_denoised=args.clip_denoised,
             model_kwargs=model_kwargs,
-            cond_fn=cond_fn,
-            device=dist_util.dev(),
+            cond_fn=cond_fn_vgg ,
+            device=th.device('cuda'),
         )
         sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
         sample = sample.permute(0, 2, 3, 1)
         sample = sample.contiguous()
+        image = Image.fromarray(sample[0].cpu().numpy())
+        image.show()
 
-        gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
-        dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
+        gathered_samples = sample
         all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
-        gathered_labels = [th.zeros_like(classes) for _ in range(dist.get_world_size())]
-        dist.all_gather(gathered_labels, classes)
+        gathered_labels = classes
         all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
         logger.log(f"created {len(all_images) * args.batch_size} samples")
 
-    arr = np.concatenate(all_images, axis=0)
+    arr = all_images
     arr = arr[: args.num_samples]
-    label_arr = np.concatenate(all_labels, axis=0)
+    label_arr = all_labels
     label_arr = label_arr[: args.num_samples]
-    if dist.get_rank() == 0:
-        shape_str = "x".join([str(x) for x in arr.shape])
-        out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
-        logger.log(f"saving to {out_path}")
-        np.savez(out_path, arr, label_arr)
+    out_path = os.path.join(logger.get_dir(), f"samples_{len(arr)}_{arr[0].shape}.npz")
+    logger.log(f"saving to {out_path}")
+    np.savez(out_path, arr, label_arr)
 
-    dist.barrier()
     logger.log("sampling complete")
 
 
